aa67b824e88a9654d411bd1634f470a7914c7daa
Christian Ascone <ascone.christian@gmail.com>
2023-07-07 11:58:20 +0200

    Setup localization

---
+++ src/i18n/ui.ts
@@ -0,0 +1,18 @@
+export const languages = {
+    en: 'English',
+    it: 'Italiano',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+    en: {
+        'nav.home': 'Home',
+        'nav.about': 'About',
+        'nav.twitter': 'Twitter',
+    },
+    it: {
+        'nav.home': 'Home',
+        'nav.about': 'Chi sono',
+    },
+} as const;
